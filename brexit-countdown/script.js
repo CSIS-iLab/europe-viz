402d356bdf6a1aec6c70966a6f2eece0eddb11e1
Tucker Harris <tucker.harris121@gmail.com>
2019-10-29 12:21:50 -0400

    Update countdown clock to new extended time of January 31, 2020

--- brexit-countdown/script.js
+++ brexit-countdown/script.js
@@ -3,46 +3,44 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-
-
 //console.log(endDate);
 
-let endD = new Date('Nov 1, 2019 00:00:00');
+let endD = new Date("Jan 31, 2020 00:00:00");
 let endDate = endD.toLocaleString(undefined, {
-  timeZone: 'Europe/Brussels'
+  timeZone: "Europe/Brussels"
 });
 
 let countDown = new Date(endDate).getTime(),
   x = setInterval(function() {
-
     let startD = new Date();
     let startDate = startD.toLocaleString(undefined, {
-      timeZone: 'Europe/Brussels'
+      timeZone: "Europe/Brussels"
     });
-
 
     let now = new Date(startDate).getTime(),
       distance = countDown - now;
-    let daysOutput = Math.floor(distance / (day));
-    let hoursOutput = Math.floor((distance % (day)) / (hour));
-    let minutesOutput = Math.floor((distance % (hour)) / (minute));
-
+    let daysOutput = Math.floor(distance / day);
+    let hoursOutput = Math.floor((distance % day) / hour);
+    let minutesOutput = Math.floor((distance % hour) / minute);
 
     function checkdigits(test) {
       if (test.toString().length == 1) {
         test = "0" + test;
-
       } else {
         test = test.toString();
       }
-      return test
+      return test;
     }
 
-    document.getElementById('days').querySelector('tspan').textContent = checkdigits(daysOutput),
-      document.getElementById('hours').querySelector('tspan').textContent = checkdigits(hoursOutput),
-      document.getElementById('minutes').querySelector('tspan').textContent = checkdigits(minutesOutput) //,
+    (document
+      .getElementById("days")
+      .querySelector("tspan").textContent = checkdigits(daysOutput)),
+      (document
+        .getElementById("hours")
+        .querySelector("tspan").textContent = checkdigits(hoursOutput)),
+      (document
+        .getElementById("minutes")
+        .querySelector("tspan").textContent = checkdigits(minutesOutput)); //,
 
     //document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-
-
-  }, second)
+  }, second);
